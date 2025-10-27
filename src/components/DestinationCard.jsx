149b13183c38330f4b92f90ae0ac1a2ede4c191b
areej-png <areejamir890@gmail.com>
2025-10-26 23:02:52 -0700

    feat: add DestinationCard component with props support

---
+++ src/components/DestinationCard.jsx
@@ -0,0 +1,12 @@
+import '../DestinationCard.css';
+function DestinationCard({ name, country, image, description }) {
+    return (
+        <div className="destination-card">
+            <img src={image} alt={name} />
+            <h2>{name}</h2>
+            <h4>{country}</h4>
+            <p>{description}</p>
+        </div>
+    );
+}
+export default DestinationCard;  
